c61b8c4c3f7d4df43db46a74025f56306f0ecd0f
K1ts0r <rostyslav.but@nure.ua>
2024-08-03 17:36:33 +0300

    Added 3 new files as start

---
+++ src/Base_concepts_of_Java.java
@@ -0,0 +1,16 @@
+public class Base_concepts_of_Java{
+    public static void main(String[] args) {
+        System.out.print("\n \t \t \nhi hi \" 11");
+    }
+}
+
+// \n  - newline
+// \t  - tab
+// \"  - вывод кавычки
+// \\  - вывод слеша
+
+// print  - однострочный вывод
+// println  - вывод с переходом на след. строку
+
+// //  - однострочный коментарий
+// /* */  - многострочный коментайрий
